5735e18a6f439e96136b3e16bb04d71c2e5a90cb
Peter Yoo <pyoo96@gmail.com>
2023-06-28 21:37:04 +0900

    perturbation function updated @ utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from tqdm import tqdm
 
+
+#######################################
+# Perturbation Related Functions
+#######################################
 
 def add_perturbation(net, radius):
+    """
+    Add random perturbation to the network with L2-norm being equal to radius.
+    """
     rand_orient = torch.randn(sum(p.numel() for p in net.parameters()), device=net.device)
     denom = random_orient.norm() / radius
     rand_vector = rand_orient / denom
@@ -15,5 +23,84 @@ def add_perturbation(net, radius):
         perturbation = perturbation.view(shape_param)
         param.data += perturbation
 
-    return
+
+def calc_perturbed_loss(net, trainloader, testloader, radius, num_samples, criterion, detailed=False):
+    """
+    Calculates the average loss value L(θ + Δ) w.r.t. random perturbation |Δ| = radius.
+    """
+    device = next(net.parameters()).device
+    theta = net.state_dict()
+    train_losses, test_losses = [], []
+    train_accs, test_accs = [], []
+
+    for _ in num_samples:
+        net.load_state_dict(theta)
+        add_perturbation(net)
+
+        with torch.no_grad():
+            train_loss, correct, total = 0., 0, 0
+
+            for images, labels in trainloader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = net(images)
+                train_loss += criterion(outputs, labels).item()
+                
+                if (detailed):
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+            train_loss, acc = train_loss / len(trainloader), 100 * correct / (total + 1e-8)
+            train_losses.append(train_loss)
+            train_accs.append(acc)
+
+            if (detailed):
+                test_loss, correct, total = 0., 0, 0
+
+                for images, labels in testloader:
+                    images, labels = images.to(device), labels.to(device)
+                    outputs = net(images)
+                    test_loss += criterion(outputs, labels).item()
+
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+                test_loss, acc = test_loss / len(testloader), 100 * correct / total
+                test_losses.append(test_loss)
+                test_accs.append(acc)
+
+    train_losses, test_losses, train_accs, test_accs = \
+            np.array(train_losses), np.array(test_losses), np.array(train_accs), np.array(test_accs)
+
+    if (not detailed):
+        return (train_losses.mean(), train_losses.std())
+    else:
+        return (train_losses.mean(), train_losses.std(), train_accs.mean(), train_accs.std(),
+                test_losses.mean(), test_losses.std(), test_losses.mean(), test_losses.std())
+
+
+def calc_solution_set_radius(net, trainloader, testloader, tau=1.0, max_radius=100, num_samples=50, num_iter=10, \
+                                criterion=nn.CrossEntropyLoss()):
+    """
+    Return |Δ| such that E[L(θ + Δ)] = τ.
+
+    max_radius: The maximum radius size that binary search will be performed on.
+    num_samples: The number of perturbation samples to estimate the expected loss value.
+    num_iter: The number of binary search iterations.
+    """
+    raise NotImplementedError()
+
+
+
+
+#######################################
+# Train, Test, Evaluate
+#######################################
+
+def train(net, trainloader)
+
+
+
+
 
